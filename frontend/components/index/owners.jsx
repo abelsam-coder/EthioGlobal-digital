d6dbb220dacfc64c6f343b3bd22f3a65c24c4f6a
Abel Samuel <abelsamuel841@gmail.com>
2026-05-21 07:44:16 +0300

    Update owners.jsx

--- frontend/components/index/owners.jsx
+++ frontend/components/index/owners.jsx
@@ -245,14 +245,14 @@ export default function Owners() {
 
   const founders = [
     {
-      img: nati,
+      img: "a",
       name: "Natinael Birhanu",
       role: "CEO & Founder",
       slogan: "Innovation isn't just about technology; it's about solving real problems for Ethiopian businesses.",
       socials: ["instagram", "telegram", "email"],
     },
     {
-      img: abel,
+      img: "b",
       name: "Nahom Samuel",
       role: "CTO & Co-founder",
       slogan: "Great code is invisible. It creates seamless digital experiences that drive our clients' growth.",
